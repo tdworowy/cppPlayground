bdf908fe61a4b5ba1bb5809c59d33f23aa26d140
Tomasz <dworowytomasz@gmail.com>
2017-09-24 15:53:12 +0200

    update

--- cppPlayground/Source1.cpp
+++ cppPlayground/Source1.cpp
@@ -5,8 +5,9 @@ int main()
 	using namespace std;
 	for (int i=0; i < 20; i++)
 	{
-		std::string s = std::to_string(i);
-		cout << "Hellow World!"+s+"\n";
+		//std::string s = std::to_string(i);
+		string s = to_string(i);
+		cout << "Hellow World!"+s << endl;
 	}
 	cin.get();
 	return 0;
