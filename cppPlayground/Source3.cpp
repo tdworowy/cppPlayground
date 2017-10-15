a7b5df42fd4abeeea00ded55386b89ee5225aa42
Tomasz <dworowytomasz@gmail.com>
2017-10-15 11:59:53 +0200

    message

---
+++ cppPlayground/Source3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+void func1(void);
+
+int main()
+{
+	while (true)
+	{	
+		func1();
+	}
+
+}
+
+
+
+void func1(void) {
+
+	using namespace std;
+	int i;
+	cout << "Enter INT: ";
+	cin >> i;
+	cout << &i;
+	cin.get();
+	cin.get();
+
+}
