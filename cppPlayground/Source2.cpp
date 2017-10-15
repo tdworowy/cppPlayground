3d592f6b2c977dd14c27944d73c4c4ae29c29cfd
Tomasz <dworowytomasz@gmail.com>
2017-10-15 11:48:09 +0200

    update

--- cppPlayground/Source2.cpp
+++ cppPlayground/Source2.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string>
+void func1(void);
+
 int main()
 {
+	func1();
+
+}
+
+
+
+void func1(void) {
+
 	using namespace std;
 	char s[256];
 	while (s != "exit") {
 		cout << "Enter String: ";
 		cin.getline(s, 256);
-	    cout << "String is: " << s << endl;
-		
+		cout << "String is: " << s << endl;
+
 	}
 
 }
