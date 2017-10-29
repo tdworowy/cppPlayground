b48d7fe2a0194737758d2bea2966058c5366a3f0
Tomasz <dworowytomasz@gmail.com>
2017-10-29 14:25:22 +0100

    update

---
+++ cppPlayground/timeCalculator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+int minutesF(long long);
+int hoursF(int);
+int daysF(int);
+
+int main()
+{
+	using namespace std;
+	long long sec;
+	int minutes;
+	int hours;
+	int days;
+
+	while (1) {
+		cout << "Enter seconds: ";
+		cin >> sec;
+		minutes = minutesF(sec);
+		hours = hoursF(minutes);
+		days = daysF(hours);
+
+		cout << "seconds: " << sec % (minutes) << endl;
+		cout << "Miutes: " << minutes % (hours) << endl;
+		cout << "hours: " << hours % (days) << endl;
+		cout << "days: " << days << endl;
+	}
+
+}
+
+int minutesF(long long seconds) {
+	return seconds / 60;
+}
+int hoursF(int minutes) {
+	return minutes / 60;
+}
+int daysF(int hours) {
+	return hours / 24;
+}
