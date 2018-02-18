3b579be3c83522f20768612d6c1db9016a2d0fea
Tomasz <dworowytomasz@gmail.com>
2018-02-18 17:25:04 +0100

    update

---
+++ cppPlayground/new_.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	using namespace std;
+	int *pointer = new int;
+
+	*pointer = 10;
+	cout << *pointer << endl;
+	cout << pointer << endl;
+	delete pointer;
+	cout << "After delete" << endl;
+	cout << *pointer << endl;
+	cout << pointer << endl;
+
+}
---
+++ cppPlayground/pointers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+	using namespace std;
+	int updates = 6;
+	int *p_updates;
+	
+	p_updates = &updates;
+
+	cout << "updates value: " << updates << endl;
+	cout << "*p_updates: " << *p_updates << endl;
+
+	cout << "address &updates: " << &updates << endl;
+	cout << "p_updates: " << p_updates << endl;
+
+	*p_updates = *p_updates + 1;
+	cout << "updates new value: " << updates << endl;
+}
