e3df68a48e0a119c11204cfd50fa3c34d70356c7
Tomasz <dworowytomasz@gmail.com>
2018-02-21 10:13:01 +0100

    update

--- cppPlayground/pointers.cpp
+++ cppPlayground/pointers.cpp
@@ -4,7 +4,7 @@ int main() {
 	using namespace std;
 	int updates = 6;
 	int *p_updates;
-	
+
 	p_updates = &updates;
 
 	cout << "updates value: " << updates << endl;
@@ -15,4 +15,19 @@ int main() {
 
 	*p_updates = *p_updates + 1;
 	cout << "updates new value: " << updates << endl;
+
+	cout << "Dinamic array" << endl;
+	cout << "Size: ";
+	int size;
+	cin >> size;
+	int * pointer_to_array = new int[size];
+	cout << "Set new value: ";
+	int value;
+	cin >> value;
+	cout << "set array index: ";
+	int index;
+	cin >> index;
+	pointer_to_array[index] = value;
+	cout << pointer_to_array[index];
+
 }
