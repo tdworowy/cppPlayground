41aba54dc6afdef951b5ea805d283eba67c93079
Tomasz <dworowytomasz@gmail.com>
2018-02-25 10:52:56 +0100

    update

---
+++ cppPlayground/struct_dynamic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+struct test1 {
+	std::string name1;
+	std::string name2;
+};
+
+int main() {
+	using namespace std;
+	test1 *ps = new test1;
+	ps->name1 = " Name1 ";
+	ps->name2 = " Name2 ";
+
+	cout << ps->name1;
+	cout << ps->name2;
+	
+
+}
