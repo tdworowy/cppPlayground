0b92a13ac039f4ec5b780fbdf41c2641f860caf0
Tomasz <dworowytomasz@gmail.com>
2018-02-27 16:59:11 +0100

    update

---
+++ cppPlayground/chars.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+	char test[13] = "cheeseburger";
+	std::cout << test << std::endl;
+}
---
+++ cppPlayground/vector_.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+int main() {
+	using namespace std;
+	vector<int> vi1(10);
+	vector<int> vi2;
+	
+}
