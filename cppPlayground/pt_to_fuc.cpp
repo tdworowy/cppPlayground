8b2e516f977adc527c8c8393face9c1b91e3996d
Tomasz <dworowytomasz@gmail.com>
2018-04-01 10:59:16 +0200

    update

---
+++ cppPlayground/pt_to_fuc.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<string>
+
+std::string func1(int);
+std::string func2(int);
+void run (std::string (*pf)(int),int);
+
+int main() {
+
+	run(func1,1);
+	run(func2,2);
+}
+
+std::string func1(int number) {
+	return "Number is: " + std::to_string(number);
+}
+std::string func2(int number) {
+	return "Number +2 is: " + std::to_string(number+2);
+}
+
+void run(std::string(*pf)(int), int arg) {
+	std::cout << pf(arg) << std::endl;
+
+}
+
