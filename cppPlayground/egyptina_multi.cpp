4a9a4be89e2e4fd5b814ff3d04c476f1f622ea72
Tomasz <dworowytomasz@gmail.com>
2018-04-07 13:34:24 +0200

    update

--- cppPlayground/egyptina_multi.cpp
+++ cppPlayground/egyptina_multi.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 
-int multi(int, int);
-bool odd(int);
-int half(int);
+long int multi(long int, long int);
+bool odd(long int);
+long int half(long int);
 
 int main() {
 	std::cout << multi(9, 9) << std::endl;
 	std::cout << multi(99, 99) << std::endl;
 	std::cout << multi(2, 2) << std::endl;
+	std::cout << multi(4, 4) << std::endl;
+	std::cout << multi(999, 999) << std::endl;
+	std::cout << multi(9999, 9999) << std::endl;
 }
-int multi(int number1, int number2) {
+long int multi(long int number1, long int number2) {
 	if (number1 == 1) return number2;
-	int result = multi(half(number1), number2 + number2);
+	long int result = multi(half(number1), number2 + number2);
 	if (odd(number1)) result = result + number2;
 	return result;
 	}
-bool odd(int number) {
+bool odd(long int number) {
 	return number & 0x1;
 }
-int half(int number) {
+long int half(long int number) {
 	return number >> 1;
 }
