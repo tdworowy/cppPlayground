546bc1f414ad7d6fe3081e953018e13e70b3f17a
Tomasz <dworowytomasz@gmail.com>
2018-05-09 16:18:06 +0200

    update

---
+++ cppPlayground/classes3_operators/mytime.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "mytime.h"
+
+Time::Time() {
+	hours = minuts = 0;
+}
+Time::Time(int h, int m) {
+	hours = h;
+	minuts = m;
+}
+void Time::addMin(int m) {
+	minuts += m;
+	hours += minuts / 60;
+	minuts %= 60;
+}
+void Time::addHr(int h) {
+	hours += h;
+}
+void Time::sum(const Time &t) const {
+	hours = h;
+	minuts = m;
+}
+Time Time::sum(const Time & t) const {
+	Time sum;
+	sum.minuts = minuts + t.minuts;
+	sum.hours = hours + t.hours + sum.minuts / 60;
+	sum.minuts %= 60;
+	return sum;
+}
+void Time::show()const {
+	std::cout << hours << " hours, " << minuts << " minuts" << std::endl;
+}
--- cppPlayground/classes3_operators/mytime.h
+++ cppPlayground/classes3_operators/mytime.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef  MYTIME_H_
+#define  MYTIME_H_
+
+class Time {
+private:
+	int hours;
+	int minuts;
+public:
+	Time();
+	Time(int h, int m = 0);
+	void addMin(int m);
+	void addHr(int h);
+	void reset(int h = 0, int m = 0);
+	Time sum(const Time & t) const;
+	void show() const;
+
+};
+
+#endif _
+
