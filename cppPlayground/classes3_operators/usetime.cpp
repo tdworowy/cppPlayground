661fefc93f777f2def3e1e6c7f0b4a8c5c60c701
Tomasz <dworowytomasz@gmail.com>
2018-08-05 12:20:52 +0200

    update

--- cppPlayground/classes3_operators/usetime.cpp
+++ cppPlayground/classes3_operators/usetime.cpp
@@ -15,26 +15,26 @@ int main() {
 	std::cout << "add" << std::endl;
 	Time total1 = coding.sum(fixing);
 	Time total2 = coding + fixing;
+	total1.show();
+	total2.show();
+
 	std::cout << "subtraction" << std::endl;
 	Time total3 = fixing - coding;
 	Time total4 = coding - fixing;
 	Time total5 = coding2 - fixing2;
 	Time total6 = Time(0, 45) - Time(0, 20);
 	Time total7 = Time(1, 0) - Time(0, 20);
-
-	Time total8 = Time(12, 30) * 10;
-	Time total9 =  10 * Time(12, 30);
-
-	Time total10 = 2 * Time(0, 36);
-	Time total11 = Time(0, 46) * 2;
-	
-	total1.show();
-	total2.show();
 	total3.show();
 	total4.show();
 	total5.show();
 	total6.show();
 	total7.show();
+
+	std::cout << "multiplication" << std::endl;
+	Time total8 = Time(12, 30) * 10;
+	Time total9 =  10 * Time(12, 30);
+	Time total10 = 2 * Time(0, 36);
+	Time total11 = Time(0, 46) * 2;
 	total8.show();
 	total9.show();
 	total10.show();
