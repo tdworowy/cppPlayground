d45c9ea9cb03cb86db2a21a67072c05a86f61c14
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-02-03 09:24:05 +0100

    add fibonacci example

---
+++ cppPlayground/math/fibonacci.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    long long a = 0;
+    long long b =  1;
+    long long temp = 0; 
+    for (long long i = 0; i < 50; i++) {
+        temp = a;
+        a = a + b;
+        b = temp;
+    }
+
+    std::cout << b << std::endl;
+}
